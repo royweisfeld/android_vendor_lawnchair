0d9cb167401bba198b86aa0209d4d13ba007a3a2
Davigamer987 <davimarchesiprimo@gmail.com>
2024-01-22 01:05:57 -0300

    lawnchair: Adapt to 14

--- lawnchair.mk
+++ lawnchair.mk
@@ -1,7 +1,7 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_COPY_FILES += \
-    vendor/lawnchair/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/lawnchair/etc/permissions/privapp-permissions-app.lawnchair.debug.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-app.lawnchair.debug.xml \
     vendor/lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/lawnchair-whitelist.xml
 
 PRODUCT_PACKAGES += \
