fdd9a592dbb03d07f6db4e5100f727a794c48991
DylanAkp <github@dylanakp.dev>
2024-09-25 23:08:43 +0200

    Lawnchair: Switch to Play Store version
    Play Signature will allow users to update easily

--- lawnchair.mk
+++ lawnchair.mk
@@ -1,8 +1,8 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_COPY_FILES += \
-    vendor/lawnchair/etc/permissions/privapp-permissions-app.lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-app.lawnchair.xml \
-    vendor/lawnchair/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/app.lawnchair-hiddenapi-package-whitelist.xml
+    vendor/lawnchair/etc/permissions/privapp-permissions-app.lawnchair.play.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-app.lawnchair.play.xml \
+    vendor/lawnchair/etc/sysconfig/app.lawnchair.play-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/app.lawnchair.play-hiddenapi-package-whitelist.xml
 
 PRODUCT_PACKAGES += \
     QuickstepSwitchOverlay \
